ffe28c5ce08326ee303f1ccb9da62b4827f77492
fazlerabbyrifat <rifatfazlerabby@gmail.com>
2023-05-02 21:32:30 +0600

    categories data created using json file

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const categories = require('./data/categories.json')
+
 
 app.get('/', (req, res) => {
     res.send('ruchi bhoj server is running');
+});
+
+app.get('/categories', (req, res) => {
+    res.send(categories);
 })
 
 app.listen(port, () => {
